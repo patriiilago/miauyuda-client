f00132eefe7942daa67ca8c119aabb4d821244c8
Teresa <tereac1102@gmail.com>
2024-03-06 11:06:29 +0100

    signupform-client component

--- src/components/SignUpFormClient/SignUpFormClient.jsx
+++ src/components/SignUpFormClient/SignUpFormClient.jsx
@@ -1,11 +1,13 @@
-import { Form, FloatingLabel, Button, Container } from 'react-bootstrap'
-import { useNavigate } from 'react-router-dom'
-
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
 
 const VITE_BASE_URL = "http://localhost:5005"
 
 
-const navigate = useNavigate()
+// const navigate = useNavigate()
 
 const handleFormSubmit = (event) => {
     event.preventDefault()
@@ -15,126 +17,101 @@ const SignUpFormClient = () => {
     return (
 
 
-        <Container className="col-md-10">
 
-            <Form onSubmit={handleFormSubmit} className="col-md-10">
-                <FloatingLabel
-                    controlId="floatingInput"
-                    label="Email address"
-                    className="mt-2 mb-2"
-                >
+        <Container>
+            <Form>
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="formGridEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="Introduce tu email" />
+                    </Form.Group>
 
-                    <Form.Control
-                        type="email"
-                        placeholder="name@example.com"
-                        value={requestData.email}
-                        onChange={handleInputChange}
-                        name={'email'}
-                    />
-                </FloatingLabel>
+                    <Form.Group as={Col} controlId="formGridPassword">
+                        <Form.Label>Contraseña</Form.Label>
+                        <Form.Control type="password" placeholder="Contraseña" />
+                    </Form.Group>
+                </Row>
 
-                <FloatingLabel controlId="floatingName" label="Name" className="mt-2 mb-2">
-                    <Form.Control
-                        type="text"
-                        placeholder="Name"
-                        value={requestData.name}
-                        onChange={handleInputChange}
-                        name={'name'}
-                    />
-                </FloatingLabel>
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="formGridName">
+                        <Form.Label>Nombre</Form.Label>
+                        <Form.Control type="text" placeholder="Introduce tu email" />
+                    </Form.Group>
 
+                    <Form.Group as={Col} controlId="formGridLastName">
+                        <Form.Label>Apellidos</Form.Label>
+                        <Form.Control type="text" placeholder="Contraseña" />
+                    </Form.Group>
+                </Row>
 
-                <FloatingLabel className="mt-2 mb-2">
-                    <Form.Select aria-label="Default select example"
-                        type="text"
-                        placeholder="Default select example"
-                        value={requestData.project}
-                        onChange={handleInputChange}
-                        name={'project'}>
-                        <option>Project: </option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
+                <Form.Group className="mb-3" controlId="formGridAddress1">
+                    <Form.Label>Dirección</Form.Label>
+                    <Form.Control placeholder="Introduce tu dirección" />
+                </Form.Group>
 
-                    </Form.Select>
-                </FloatingLabel>
+                <Form.Group className="mb-3" controlId="formGridAddress2">
+                    <Form.Label>Detalles dirección</Form.Label>
+                    <Form.Control placeholder="Piso, estudio, apartamento..." />
+                </Form.Group>
 
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="formGridCity">
+                        <Form.Label>Ciudad</Form.Label>
+                        <Form.Control />
+                    </Form.Group>
 
+                    <Form.Group as={Col} controlId="formGridState">
+                        <Form.Label>País</Form.Label>
+                        <Form.Select defaultValue="Choose...">
+                            <option>España</option>
+                            <option>Portugal</option>
+                            <option>Francia</option>
+                            <option>Italia</option>
+                            <option>Grecia</option>
+                            <option>Mexico</option>
+                            <option>Colombia</option>
+                        </Form.Select>
+                    </Form.Group>
 
-                <FloatingLabel className="mt-2 mb-2">
-                    <Form.Select aria-label="Default select example"
-                        type="text"
-                        placeholder="Default select example"
-                        value={requestData.type}
-                        onChange={handleInputChange}
-                        name={'type'}>
-                        <option>Request type: </option>
-                        <option value="Doubt">Doubt</option>
-                        <option value="Help">Help</option>
-                        <option value="Technical Assistance">Technical Assistance</option>
-                    </Form.Select>
-                </FloatingLabel>
+                    <Form.Group as={Col} controlId="formGridZip">
+                        <Form.Label>C.P.</Form.Label>
+                        <Form.Control />
+                    </Form.Group>
+                </Row>
 
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="formGridNumberPets">
+                        <Form.Label>Número de mascotas</Form.Label>
+                        <Form.Control type="number" placeholder="Número de mascotas" />
+                    </Form.Group>
 
-                <FloatingLabel className="mt-2 mb-2">
-                    <Form.Select aria-label="Default select example"
-                        type="text"
-                        placeholder="Default select example"
-                        value={requestData.tags}
-                        onChange={handleInputChange}
-                        name={'tags'}>
-                        <option>Tags: </option>
-                        <option value="JavaScript">JavaScript</option>
-                        <option value="Async programming">Async programming</option>
-                        <option value="Reactjs">Reactjs</option>
-                        <option value="Phyton">Phyton</option>
-                        <option value="Jupiter">Jupiter</option>
-                        <option value="Panda">Panda</option>
-                    </Form.Select>
-                </FloatingLabel>
+                    <Form.Group as={Col} controlId="formGridStateAnimals">
+                        <Form.Label>Tipo de mascota</Form.Label>
+                        <Form.Select defaultValue="Choose...">
+                            <option>Perro</option>
+                            <option>Gato</option>
+                            <option>Conejo</option>
+                            <option>Pájaro</option>
+                            <option>Reptil</option>
+                            <option>Hurón</option>
+                            <option>Otros</option>
+                        </Form.Select>
+                    </Form.Group>
+                </Row>
 
+                <Form.Group className="mb-3" id="formGridCheckbox">
+                    <Form.Check type="checkbox" label="He leído y acepto los términos y condiciones." />
+                </Form.Group>
 
-
-
-
-
-                <FloatingLabel controlId="floatingTitleTarea" label="title" className="mt-2 mb-2">
-
-                    <Form.Control
-                        type="text"
-                        placeholder="Title"
-                        value={requestData.title}
-                        onChange={handleInputChange}
-                        name={'title'}
-
-                    />
-                </FloatingLabel>
-
-
-
-                <FloatingLabel controlId="floatingTextarea2" label="Description" className="mt-2 mb-2" >
-
-                    <Form.Control
-                        type="text"
-                        as="textarea"
-                        placeholder="Leave a comment here"
-                        value={requestData.description}
-                        onChange={handleInputChange}
-                        name={'description'}
-                        style={{ height: '100px' }}
-                    />
-                </FloatingLabel>
-
-
-
-                <Button variant="dark mb-5" type="submit" value='New Request'>New Request</Button>
-
+                <Button variant="dark" type="submit">
+                    Enviar
+                </Button>
             </Form>
-
         </Container>
-
-    )
+    );
 }
+
+
 
 
 export default SignUpFormClient
