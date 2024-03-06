cc0ddf18b570ba0e2fb81489510c1613d1314518
Teresa <tereac1102@gmail.com>
2024-03-06 18:54:36 +0100

    petform and signupformclient finished

--- src/components/PetForm/PetForm.jsx
+++ src/components/PetForm/PetForm.jsx
@@ -1,32 +1,82 @@
 import { Accordion, Button, Form, Col, Row } from "react-bootstrap"
 import '../../components/PetForm/PetForm.css'
+import { useState } from "react"
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
+
+const VTE_SERVER_URL = "http://localhost:5005"
 
 
 const PetForm = () => {
+
+    const [petData, setPetData] = useState({
+        name: "",
+        chipOwner: "",
+        type: "",
+        weight: "",
+        breed: "",
+        birth: "",
+        sex: "",
+        chipNumber: "",
+    })
+
+    const navigate = useNavigate()
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault()
+
+        axios
+            .post(`${VTE_SERVER_URL}/api/auth/signup`, petData)
+            .then(() => navigate('/'))
+            .catch(err => console.log(err))
+
+    }
+
+    const handleInputChange = (event) => {
+        const { value, name } = event.target
+        setPetData({ ...petData, [name]: value })
+
+    }
+
 
     return (
 
         <Accordion defaultActiveKey="1">
             <Accordion.Item eventKey="0">
-                <Accordion.Header variant="dark"><strong>Asegurar mascota:</strong> </Accordion.Header>
+                <Accordion.Header onSubmit={handleFormSubmit} variant="dark"><strong>Asegurar mascota:</strong> </Accordion.Header>
                 <Accordion.Body>
 
                     <Row className="mb-3">
                         <Form.Group as={Col} variant="dark" className="mb-3" controlId="formGridPetName">
                             <Form.Label>Nombre:</Form.Label>
-                            <Form.Control placeholder="Nombre de tu mascota" />
+                            <Form.Control
+                                placeholder="Nombre de tu mascota"
+                                onChange={handleInputChange}
+                                value={petData.name}
+                                name={"name"}
+                            />
                         </Form.Group>
 
                         <Form.Group as={Col} variant="dark" className="mb-3" controlId="formGridPetOwner">
                             <Form.Label>Nombre propietari@:</Form.Label>
-                            <Form.Control placeholder="Titutar de la mascota" />
+                            <Form.Control
+                                placeholder="Titutar de la mascota"
+                                onChange={handleInputChange}
+                                value={petData.chipOwner}
+                                name={"chipOwner"}
+                            />
                         </Form.Group>
                     </Row>
 
                     <Row className="mb-3">
                         <Form.Group as={Col} variant="dark" controlId="formGridStateAnimals">
                             <Form.Label>Tipo de mascota:</Form.Label>
-                            <Form.Select defaultValue="Choose...">
+                            <Form.Select
+                                onChange={handleInputChange}
+                                value={petData.type}
+                                name={"type"}
+                            >
                                 <option>Perro</option>
                                 <option>Gato</option>
                                 <option>Conejo</option>
@@ -39,26 +89,47 @@ const PetForm = () => {
 
                         <Form.Group as={Col} controlId="formGridWeight">
                             <Form.Label>Peso en Kg:</Form.Label>
-                            <Form.Control type="number" placeholder="Peso de la mascota" />
+                            <Form.Control
+                                type="number"
+                                placeholder="Peso de la mascota"
+                                onChange={handleInputChange}
+                                value={petData.weight}
+                                name={"weight"}
+                            />
                         </Form.Group>
                     </Row>
 
                     <Row className="mb-3">
                         <Form.Group as={Col} variant="dark" controlId="formGridStatebreed ">
                             <Form.Label>Raza:</Form.Label>
-                            <Form.Control type="text" placeholder="Raza de la mascota" />
+                            <Form.Control
+                                type="text"
+                                placeholder="Raza de la mascota"
+                                onChange={handleInputChange}
+                                value={petData.breed}
+                                name={"breed"}
+                            />
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="formGridBirth">
                             <Form.Label>Fecha de nacimiento:</Form.Label>
-                            <Form.Control type="date" placeholder="Fecha de nacimiento" />
+                            <Form.Control
+                                type="date" placeholder="Fecha de nacimiento"
+                                onChange={handleInputChange}
+                                value={petData.birth}
+                                name={"birth"}
+                            />
                         </Form.Group>
                     </Row>
 
                     <Row className="mb-3">
                         <Form.Group as={Col} variant="dark" controlId="formGridStatePetSex">
                             <Form.Label>Sexo:</Form.Label>
-                            <Form.Select defaultValue="Choose...">
+                            <Form.Select
+                                onChange={handleInputChange}
+                                value={petData.sex}
+                                name={"sex"}
+                            >
                                 <option>Macho</option>
                                 <option>Hembra</option>
                             </Form.Select>
@@ -66,12 +137,24 @@ const PetForm = () => {
 
                         <Form.Group as={Col} controlId="formGridChip">
                             <Form.Label>Número de Chip:</Form.Label>
-                            <Form.Control type="number" placeholder="Peso de la mascota" />
+                            <Form.Control
+                                type="number"
+                                placeholder="Peso de la mascota"
+                                onChange={handleInputChange}
+                                value={petData.chipNumber}
+                                name={"chipNumber"}
+                            />
                         </Form.Group>
                     </Row>
 
 
-                    <Button type="submit" className="mt-2 mb-2" variant="dark">Guardar mascota</Button>
+                    <Button
+                        onClick={handleFormSubmit}
+                        type="submit"
+                        className="mt-2 mb-2"
+                        variant="dark">
+                        Guardar mascota
+                    </Button>
 
                 </Accordion.Body>
             </Accordion.Item>
