0504083e71250d83c79f5fe6df2578368aea0b27
Teresa <tereac1102@gmail.com>
2024-03-07 16:13:35 +0100

    clientcard

--- src/components/ClientCard/ClientCard.jsx
+++ src/components/ClientCard/ClientCard.jsx
@@ -1,28 +1,47 @@
-import { Container } from "react-bootstrap";
+import { Button, Card } from 'react-bootstrap'
 
-function ClientCard() {
+const ClientCard = ({
+
+    firstName,
+    lastName,
+    phone,
+    email,
+    password,
+    image,
+    address,
+    street,
+    zipCode,
+    city,
+    country,
+    location,
+    coordinates,
+    longitude,
+    latitude
+
+}) => {
     return (
-        <Container>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                    </Card.Text>
-                </Card.Body>
-                <ListGroup className="list-group-flush">
-                    <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                    <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                    <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-                </ListGroup>
-                <Card.Body>
-                    <Card.Link href="#">Card Link</Card.Link>
-                    <Card.Link href="#">Another Link</Card.Link>
-                </Card.Body>
-            </Card>
-        </Container>
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card.Body>
+
+                <Card.Title>
+                    <h1>{firstName}</h1>
+                    <br />
+                    <h2>{lastName}</h2>
+                </Card.Title>
+
+                <Card.Text>
+                    <strong>Datos de contacto:</strong>
+                    <br />
+                    <strong>Teléfono: </strong>{phone}
+                    <strong>Email: </strong>{email}
+                    <strong>Dirección: </strong>{street} {zipCode} {city} {country}
+
+                </Card.Text>
+                <Button variant="dark"><strong> Responder</strong> </Button>
+            </Card.Body>
+        </Card>
+
     )
 }
 export default ClientCard
