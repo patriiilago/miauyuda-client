80f6e44f80ca9299c95970df1b3c9d28bb6fb228
Oscar Gomez Diez <oscarfotodiez@gmail.com>
2024-03-07 18:28:00 +0100

    solving nabvar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Button, Nav, Navbar, Modal } from 'react-bootstrap'
+import { Button, Nav, Navbar, Modal, Row, Col } from 'react-bootstrap'
 import { NavLink, Link } from "react-router-dom"
 import Container from 'react-bootstrap/Container';
 import { useContext, useState } from "react";
@@ -10,8 +10,11 @@ function Navigation(handleLoginSubmit) {
     const { user, isLoggedIn, logout } = useContext(AuthContext)
 
     const [show, setShow] = useState(false);
+    const [modalShow, setModalShow] = useState(false)
     // const navigate = useNavigate()
 
+    const handleModalShow = () => setModalShow(true)
+    const handleModalClose = () => setModalShow(false)
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
@@ -74,8 +77,8 @@ function Navigation(handleLoginSubmit) {
                             !isLoggedIn && (
                                 <>
 
-                                    <Link to={`/signup`}>
-                                        <Button variant="dark" className='btn-signup' >Crear Cuenta</Button>
+                                    <Link>
+                                        <Button variant="dark" className='btn-signup' onClick={handleModalShow}>Crear Cuenta</Button>
                                     </Link>
 
                                     <Link to={`/login`}>
@@ -98,6 +101,32 @@ function Navigation(handleLoginSubmit) {
                     <LoginForm handleClose={() => setShow(false)} />
                 </Modal.Body>
             </Modal>
+
+
+            <Modal show={modalShow}>
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Darte de alta como:
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="grid-example">
+                    <Container>
+                        <Row>
+                            <Col xs={9} md={6}>
+                                <img src="https://asset.cloudinary.com/dxfey6stw/f2f2aacc880a3d6ea9c644b69cdb219f" alt="imagen cliente" />
+                            </Col>
+                            <Col xs={9} md={6}>
+                                <img src="https://asset.cloudinary.com/dxfey6stw/b3db1282fa24e6a8acaaf9ffad8f9079" alt="imagen profesional" />
+                            </Col>
+                        </Row>
+                    </Container>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={handleModalClose}>Close</Button>
+                </Modal.Footer>
+            </Modal>
+
+
 
         </Navbar >
     )
