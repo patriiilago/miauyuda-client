4b34d8ef70e43291b293a22f6fe0a9eb1f60a717
Teresa <tereac1102@gmail.com>
2024-03-07 19:39:32 +0100

    starting pages

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
+import Footer from "../../components/Footer/Footer"
+
 const HomePage = () => {
 
 
 
     return (
         <div className="HomePage">
-
+            <Footer />
 
         </div >
     )
--- src/Pages/NotFoundPage/NotFoundPage.jsx
+++ src/Pages/NotFoundPage/NotFoundPage.jsx
@@ -1,13 +1,27 @@
+import { Link } from "react-router-dom"
+import Button from 'react-bootstrap/Button'
+import './../NotFoundPage/NotFoundPage.css'
+
+const img = 'https://ih1.redbubble.net/image.492975046.7150/flat,750x,075,f-pad,750x1000,f8f8f8.u1.jpg'
+
 const NotFoundPage = () => {
 
 
-
     return (
-        <div className="NotFoundPage">
+        <>
 
-
-        </div >
+            <div className="NotFoundPage">
+                <img className="notfoundimage" src={img} alt="Gato llorando" />
+                <Link to={"/"}>
+                    <Button
+                        className="btn btn-dark"
+                        size="sg"
+                    >
+                        Volver a Inicio
+                    </Button>
+                </Link>
+            </div>
+        </>
     )
 }
-
 export default NotFoundPage
