ca4ad6f47936c76d74980f684f90ff5598c7f763
Teresa <tereac1102@gmail.com>
2024-03-10 15:21:58 +0100

    solved signupformprofessional

--- src/components/SignUpFormProfessional/SignUpFormProfessional.jsx
+++ src/components/SignUpFormProfessional/SignUpFormProfessional.jsx
@@ -25,7 +25,7 @@ const SignUpFormProfessional = () => {
         coordinates: [Number],
         schedule: "",
         specialty: "",
-        emergencies: false,
+        emergencies: "",
         role: "professional"
 
 
@@ -249,8 +249,8 @@ const SignUpFormProfessional = () => {
                             onChange={handleInputChange}
                             type="text"
                             placeholder="urgencias"
-                            value={professionalData.urgencies}
-                            name={"urgencies"}
+                            value={professionalData.emergencies}
+                            name={"emergencies"}
                         >
                             <option value={true}>Sí</option>
                             <option value={false}>No</option>
