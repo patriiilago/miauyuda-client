ed1603225359a86b199248ca0dfd3697046378d0
Oscar Gomez Diez <oscarfotodiez@gmail.com>
2024-04-09 20:33:56 +0200

    profile page

--- src/Pages/ClientProfilePage/ClientProfilePage.jsx
+++ src/Pages/ClientProfilePage/ClientProfilePage.jsx
@@ -6,13 +6,15 @@ import './ClientProfilePage.css'
 import RequestsList from "../../components/RequestsList/RequestsList.jsx"
 import { AuthContext } from "../../context/auth.context.jsx"
 import requestServices from "../../services/request.services.js"
-import { Row, Col, Container } from "react-bootstrap"
+import { Row, Col, Button } from "react-bootstrap"
+import { Link } from "react-router-dom"
 
 const ClientProfilePage = () => {
 
     const [client, setClient] = useState([])
     const [requests, setRequests] = useState([])
     const { user } = useContext(AuthContext)
+    const [selectedCard, setSelectedCard] = useState('profile')
 
     useEffect(() => {
         loadClient()
@@ -32,24 +34,24 @@ const ClientProfilePage = () => {
             .then(({ data }) => setRequests(data))
             .catch(err => console.log(err))
     }
-
+    const handleSidebarButtonClick = (cardType) => {
+        setSelectedCard(cardType)
+    }
     return (
-        <Container>
-            <Row className="UserProfilePage">
-                <Col md={4}>
-                    <h1 className="profileTitle">Datos de tu perfil:</h1>
-                    <ClientCard {...client} />
-                </Col>
-                <Col md={4}>
-                    <h1 className="profileTitle">Datos de tu mascota:</h1>
-                    <PetCard {...client.pet} />
-                </Col>
-                <Col md={4}>
-                    <h1 className="profileTitle">Tus consultas:</h1>
-                    <RequestsList requests={requests} loadRequests={loadRequests} />
-                </Col>
-            </Row>
-        </Container>
+        <Row className="UserProfilePage">
+            <Col className="sideBar" md='3'>
+                <h2 className="sideBarTitle">Mi cuenta</h2>
+                <hr />
+                <Link className="buttonSideBar" onClick={() => handleSidebarButtonClick('profile')}><img className="imageProfile" src="https://res.cloudinary.com/dxfey6stw/image/upload/v1712680681/ziqu6bt6fswnlwvq814c.png" alt="profile image" />Mi perfil</Link>
+                <Link className="buttonSideBar" onClick={() => handleSidebarButtonClick('pets')}> <img src="https://res.cloudinary.com/dxfey6stw/image/upload/v1712680681/ugaynodhmypjeifobqxo.svg" alt="pet image" />Mis mascotas</Link>
+                <Link className="buttonSideBar" onClick={() => handleSidebarButtonClick('requests')}><img src="https://res.cloudinary.com/dxfey6stw/image/upload/v1712680681/uwyluzpg2cdajvm7ffyo.svg" alt="notification image" />Mis consultas</Link>
+            </Col>
+            <Col md='8'>
+                {selectedCard === 'profile' && <ClientCard {...client} />}
+                {selectedCard === 'pets' && <PetCard {...client.pet} />}
+                {selectedCard === 'requests' && <RequestsList requests={requests} loadRequests={loadRequests} />}
+            </Col>
+        </Row>
 
     )
 }
--- src/components/ClientCard/ClientCard.jsx
+++ src/components/ClientCard/ClientCard.jsx
@@ -1,4 +1,4 @@
-import { Button, Card } from 'react-bootstrap'
+import { Button, Card, Row, Col } from 'react-bootstrap'
 import './../../components/ClientCard/ClientCard.css'
 import { Link } from 'react-router-dom'
 
@@ -13,23 +13,58 @@ const ClientCard = ({
 
 
     return (
-        <Card className='ClientCard h-80' >
-            <Card.Img className='profilepic' variant="top" src={image} alt={`${firstName} ${lastName}`} />
-            <Card.Body>
-                <Card.Title className='ClientTitle'>
-                    <h1>{firstName} {lastName}</h1>
-                </Card.Title>
-                <Card.Body>
-                    <p className='clientData'><strong>Datos de contacto:</strong></p>
-                    <p className='clientData'><strong>📞 Teléfono: </strong>{phone}</p>
-                    <p className='clientData'><strong>💻 Email: </strong>{email}</p>
-                    <p className='clientData'><strong>📍 Dirección: </strong> {address}</p>
-                </Card.Body>
-                <Link to={`/petform`}>
-                    <Button as={'span'}>Añadir mascota</Button>
-                </Link>
-            </Card.Body>
-        </Card>
+        <>
+            <Row className='  '>
+                <h2 className='headerProfile'>Mi Perfil</h2>
+                <p className='headerProfile1'>Aquí puedes consultar todos tus datos personales</p>
+                <Col md='5'>
+                    <img className='userImage' src={image} alt={`${firstName} ${lastName}`} />
+                </Col>
+                <Col className='personalData' md='7'>
+                    <h3>Datos personales</h3>
+                    <span className='data'>Nombre</span>
+                    <span className='holder'>{firstName}</span>
+                    <span className='data'>Apellidos</span>
+                    <span className='holder'>{lastName}</span>
+                    <span className='data'>Email</span>
+                    <span className='holder'>{email}</span>
+                </Col>
+            </Row >
+            <Row className='contactData'>
+                <Col className='personalData' md='12'>
+                    <h3>Datos de contacto</h3>
+                    <span className='data'>Dirección</span>
+                    <span className='holder'>{address}</span>
+                    <span className='data'>Teléfono</span>
+                    <span className='holder'>{phone}</span>
+                    <span className='data'>Ciudad</span>
+                    <span className='holder'>{email}</span>
+                    <span className='data'>Código Postal</span>
+                    <span className='holder'>{email}</span>
+                    <span className='data'>País</span>
+                    <span className='holder'>{email}</span>
+
+                </Col>
+            </Row >
+        </>
+
+        // <Card className='ClientCard h-80' >
+        //     <Card.Img className='profilepic' variant="top" src={image} alt={`${firstName} ${lastName}`} />
+        //     <Card.Body>
+        //         <Card.Title className='ClientTitle'>
+        //             <h1>{firstName} {lastName}</h1>
+        //         </Card.Title>
+        //         <Card.Body>
+        //             <p className='clientData'><strong>Datos de contacto:</strong></p>
+        //             <p className='clientData'><strong>📞 Teléfono: </strong>{phone}</p>
+        //             <p className='clientData'><strong>💻 Email: </strong>{email}</p>
+        //             <p className='clientData'><strong>📍 Dirección: </strong> {address}</p>
+        //         </Card.Body>
+        //         <Link to={`/petform`}>
+        //             <Button as={'span'}>Añadir mascota</Button>
+        //         </Link>
+        //     </Card.Body>
+        // </Card>
     )
 }
 export default ClientCard
