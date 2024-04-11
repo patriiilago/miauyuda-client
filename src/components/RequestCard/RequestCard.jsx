83ae96a4a998a1fc60cedd9f87167cfd3c7915b7
Oscar Gomez Diez <oscarfotodiez@gmail.com>
2024-04-11 15:50:21 +0200

    solving issues

--- src/components/RequestCard/RequestCard.jsx
+++ src/components/RequestCard/RequestCard.jsx
@@ -4,15 +4,10 @@ import { useContext } from 'react'
 import { AuthContext } from '../../context/auth.context'
 import ResponseForm from '../ResponseForm/ResponseForm'
 import { Link } from 'react-router-dom'
-import { useState } from 'react'
 //testing
-const RequestCard = ({ client, question, response, _id, loadRequests, image }) => {
+const RequestCard = ({ client, question, response, _id, loadRequests, image, professional }) => {
 
-    const [expanded, setExpanded] = useState(false);
     const { user } = useContext(AuthContext)
-    const toggleExpand = () => {
-        setExpanded(!expanded);
-    };
 
     return (
         <>
@@ -20,25 +15,56 @@ const RequestCard = ({ client, question, response, _id, loadRequests, image }) =
                 <Col md='6'>
                     <div className='profileClient'>
                         <img className='clientImage' type="file" src={client.image} />
-                        <h1>{client.firstName}</h1>
-                        <span>tiene una pregunta que hacerte</span>
-                        <div >
+                        <div>
+                            <h1>{client.firstName}</h1>
+                            <span>tiene una pregunta que hacerte</span>
                         </div>
                     </div>
-                </Col>
-                <Col md='6'>
-                    <h3>Tu consulta:</h3>
-                    <hr />
-                    <span>{question}</span>
-                    <img
-                        className={`req-img ${expanded ? 'expanded' : ''}`}
-                        src={image}
-                        alt="Imagen"
-                        onClick={toggleExpand}
-                    />
+                    <span className='questionResponseTitle'>Tu consulta:</span>
+                    <hr className='questionrh' />
+                    <span className='questionText'>{question}</span>
+                    <img className='question-img' src={image} alt="Imagen" />
                     <Link to={`/clients/${client._id}`} className="buttonProfile" >
                         Ver perfil
                     </Link>
+                </Col>
+                <Col md='6'>
+                    <div className='profileProfesional'>
+                        <div>
+                            <h1>{professional.firstName}</h1>
+                            <span>tiene una pregunta que hacerte</span>
+                        </div>
+                        <img className='profesionalImage' type="file" src={professional.image} />
+                    </div>
+                    <ListGroup className="list-group-flush">
+                        {
+                            user.role === 'Professional' && response === '' &&
+                            <>
+                                <ResponseForm requestId={_id} loadRequests={loadRequests} />
+                            </>
+                        }
+
+                        {
+                            user.role === 'Professional' && response != '' &&
+                            <>
+                                <span className='questionResponseTitle'><img className='imageResponse' src="https://res.cloudinary.com/dxfey6stw/image/upload/v1712791972/qud1rs4rl1sdhewd1yhw.png" alt="checked image" />​ Respuesta del experto: </span>{response}
+                            </>
+                        }
+
+                        {
+                            user.role === 'Client' && response === '' &&
+                            <>
+                                <p><strong><img className='imageResponse' src="https://res.cloudinary.com/dxfey6stw/image/upload/v1712791972/gztu5fqjt3xthqcnaal9.png" alt="clock image" />​​ Aún sin respuesta, en breve te contestará un profesional...</strong></p>
+                            </>
+                        }
+
+                        {
+                            user.role === 'Client' && response != '' &&
+                            <>
+                                <h3><img className='imageResponse' src="https://res.cloudinary.com/dxfey6stw/image/upload/v1712791972/qud1rs4rl1sdhewd1yhw.png" alt="checked image" />​Respuesta del experto: </h3>{response}
+                            </>
+                        }
+                    </ListGroup>
                 </Col>
             </Row>
 
