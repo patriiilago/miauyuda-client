3f002297726db7a730f59dd6e6befce1deeb5799
Patri <patrilagoespino@gmail.com>
2026-02-15 14:34:02 +0100

    feat: redirect 'Saber más' buttons to specific FAQ sections using hash links

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -93,7 +93,7 @@ const HomePage = () => {
                                 </Accordion.Body>
                             </Accordion.Item>
                         </Accordion>
-                        <Link to="/questions" className="button-faqs">Saber más</Link>
+                        <Link to="questions#perros" className="button-faqs">Saber más</Link>
                     </div>
                 </Col>
                 <Col md='6' className="img-col-center">
@@ -140,7 +140,7 @@ const HomePage = () => {
                                 </Accordion.Body>
                             </Accordion.Item>
                         </Accordion>
-                        <Link to="/questions" className="button-faqs">Saber más</Link>
+                        <Link to="/questions#gatos" className="button-faqs">Saber más</Link>
                     </div>
                 </Col>
             </Row>
@@ -196,7 +196,7 @@ const HomePage = () => {
                                 </Accordion.Body>
                             </Accordion.Item>
                         </Accordion>
-                        <Link to="/questions" className="button-faqs">Saber más</Link>
+                        <Link to="/questions#exoticos" className="button-faqs">Saber más</Link>
                     </div>
                 </Col>
                 <Col md='6' className="img-col-center">
@@ -206,142 +206,6 @@ const HomePage = () => {
                 </Col>
 
             </Row>
-
-
-            {/* <Row className="pug-row-600">
-                <Col md='6' className="img-col-center">
-                    <img className="rowtxt1" src='https://res.cloudinary.com/dxfey6stw/image/upload/v1712579100/vcpbfa4crh0klorzqdsx.png' alt="dog-photo" />
-                </Col>
-                <Col md='6' className="dogQuestions">
-                    <p className="rowTitle"><span className="sharedlovetxt1">Sobre tus </span> <span className="rowtxt2">perros</span></p>
-                    <div>
-                        <Accordion defaultActiveKey="0">
-                            <Accordion.Item eventKey="0">
-                                <Accordion.Header>Tengo un cachorro, ¿Lo puedo sacar a la calle?</Accordion.Header>
-                                <Accordion.Body>
-                                    La razón principal que los propietarios de cachorros deben conocer antes de sacarlos a la calle por primera vez, es que el animal se encuentre correctamente protegido frente a las enfermedades infecciosas más comunes de ésa etapa. Recuerda que su sistema inmunitario aún se está desarrollando y por tanto es fundamental que acuerdes el programa antiparasitario y vacunal más adecuado para él/ella en tú veterinario de confianza.
-                                </Accordion.Body>
-                            </Accordion.Item>
-                            <Accordion.Item eventKey="1">
-                                <Accordion.Header>¿Es la esterilización una buena idea?</Accordion.Header>
-                                <Accordion.Body>
-                                    La esterilización de nuestras mascotas siempre va suponer un beneficio en el que todos saldremos ganando. Una vez has decidido que no quieres a tu animal como reproductor, debes esterilizarlo. La esterilización es un acto quirúrgico serio, con el que se pueden evitar enfermedades graves tales como tumores mamarios, infecciones uterinas ó estrés por embarazo psicológico en las hembras; tumores testiculares, de próstata ó trastornos graves de comportamiento en el caso de los machos.
-                                </Accordion.Body>
-                            </Accordion.Item>
-                            <Accordion.Item eventKey="2">
-                                <Accordion.Header>¿Necesita mi perro una limpieza de boca?</Accordion.Header>
-                                <Accordion.Body>
-                                    La enfermedad periodontal, es uno de los procesos más comunes en perros y gatos adultos. El mal aliento, es uno de los primeros signos de que puede estar padeciendo enfermedad dental. Sin un tratamiento adecuado, podemos encontrar problemas como abscesos de raíz, caries, e incluso incomodidad al comer. El papel del veterinario es fundamental para detectar el problema a tiempo. Él revisará la cantidad de sarro ó gingivitis presente y planteará una higiene bucal adecuada, siempre bajo anestesia general.
-                                </Accordion.Body>
-                            </Accordion.Item>
-                            <Accordion.Item eventKey="3">
-                                <Accordion.Header>¿Cuál es la raza mas adecuada para mí?</Accordion.Header>
-                                <Accordion.Body>
-                                    Pedigrí, Cruce ó Mestizo. Cachorro ó Adulto. Hembra ó Macho. Son muchas las dudas que se nos plantean. Aunque siempre te vamos a recomendar la adopción por encima de la compra, es cierto que escoger un perro de raza nos aporta un grado de predictibilidad acerca del tamaño, longitud, pelaje incluso algunas dolencias que puede padecer. La raza de perro más adecuada a tu caso, dependerá principalmente de tu estilo de vida. Tú veterinario más cercano será el asesor principal que puedes encontrar antes de tomar la decisión.
-                                </Accordion.Body>
-                            </Accordion.Item>
-                        </Accordion>
-                        <Link to="/questions" className="button-faqs">Saber más</Link>
-                    </div>
-                </Col>
-            </Row>
-
-            <Row className="cat-row-600">
-                <Col md='6' className="img-col-center">
-                    <img className="cat-img-600" src='https://res.cloudinary.com/dxfey6stw/image/upload/v1712579117/mtwbjxihqcnabgc4znyu.png' alt="cat-photo" />
-                </Col>
-                <Col md='6' className="catQquestions">
-                    <p className="rowTitle"><span className="rowtxt1">Sobre tus </span> <span className="rowtxt2">gatos</span></p>
-                    <div>
-                        <Accordion className="catAccordion" defaultActiveKey="0">
-                            <Accordion.Item eventKey="0">
-                                <Accordion.Header>Tengo un gatito por primera vez ¿como le puedo enseñar a usar la arena?</Accordion.Header>
-                                <Accordion.Body>
-                                    Puedes mostrarle la caja primero para que se acostumbre a su presencia. Después prueba a colocarlo allí al despertar y después de comer y beber. La caja de arena debe estar siempre alejada del lugar donde come y en un lugar silencioso y tranquilo, a veces incluso cubierta total ó parcialmente. Recuerda que siempre debes seguir ésta fórmula en casa para prevenir enfermedades frecuentes de tracto urinario: tantas cajas de arena, como gatos que haya en la casa +1 y debes mantenerla limpia diariamente.
-                                </Accordion.Body>
-                            </Accordion.Item>
-                            <Accordion.Item eventKey="1">
-                                <Accordion.Header>¿Cual es la mejor raza de gato para personas alérgicas?</Accordion.Header>
-                                <Accordion.Body>
-                                    No existe un gato hipoalergénico como tal. Es más, debes estar seguro a qué tienes alergia exactamente. Si el pelo es un problema, evitaría cualquier raza de pelo largo. En este sentido Sphynx, es la única raza de gato sin pelo.
-                                </Accordion.Body>
-                            </Accordion.Item>
-                            <Accordion.Item eventKey="2">
-                                <Accordion.Header>¿Qué puedo hacer si mi gato orina o defeca fuera de la bandeja?</Accordion.Header>
-                                <Accordion.Body>
-                                    Es posible que sea por un problema de aversión con la bandeja o el sustrato (arena). Sin embargo puede que no sea un problema con la bandeja/arena y el gato padezca algún tipo de patología. Esto es más probable si vemos que tiene dificultad al orinar, va más veces a la bandeja, pasa más tiempo en ella o detectamos sangre en su orina. En estos casos es altamente recomendable acudir a la clínica para investigar la patología que padece el animal. Así evitamos que entre otras consecuencias el gato acabe obstruyéndose y se complique el problema.
-                                </Accordion.Body>
-                            </Accordion.Item>
-                            <Accordion.Item eventKey="3">
-                                <Accordion.Header>¿Qué plantas pueden ser tóxicas para mi gato?</Accordion.Header>
-                                <Accordion.Body>
-                                    Los gatos tienen por costumbre mordisquear diversos vegetales. El problema es que existen multitud de plantas que son tóxicas para ellos. Unas producen alteraciones gastrointestinales como la Flor de Pascua. Otras como las Lileáceas son altamente tóxicas y pueden causarles la muerte por fallo renal. Si tienes plantas y gatos en casa, asegúrate que éstas no son tóxicas para ellos. Comprueba el listado de plantas tóxicas en internet. Te recomendamos que las busques por su nombre científico. Si sospechas que tu gato ha podido ingerir alguna planta tóxica ponte en contacto con nosotros de forma urgente.
-                                </Accordion.Body>
-                            </Accordion.Item>
-                        </Accordion>
-                        <Link to="/questions" className="button-faqs">Saber más</Link>
-                    </div>
-                </Col>
-            </Row>
-
-            <Row className="exotic-row-600">
-                <Col md='6' className="img-col-center" >
-                    <img className="exotic-img-600" src='https://res.cloudinary.com/dxfey6stw/image/upload/v1712579100/hcisbmbf3buvqv8tzeai.png' alt="exotic-animal-photo" />
-                </Col>
-                <Col md='6' className="exoticQuestions">
-                    <p className="rowTitle"><span className="rowtxt1">Sobre tus </span> <span className="rowtxt2">animales exóticos</span></p>
-                    <div>
-                        <Accordion defaultActiveKey="0">
-                            <Accordion.Item eventKey="0">
-                                <Accordion.Header>¿Que puedo hacer con un pájaro herido?</Accordion.Header>
-                                <Accordion.Body>
-                                    Cuando te encuentras a un pájaro herido, es muy común pensar que el animal necesita de tus cuidados y de tu protección para salir adelante. Ahora, hay que destacar que no siempre es así. De este modo, podrías poner en peligro la vida de este ser si lo mueves del lugar en el que te lo hayas encontrado. Por ello, lo primero que debes hacer es ponerte en contacto con un centro de recuperación de fauna silvestre. En el caso de que no conozcas ninguno, será tan sencillo como llamar al SEPRONA a través del número 112.
-                                </Accordion.Body>
-                            </Accordion.Item>
-                            <Accordion.Item eventKey="1">
-                                <Accordion.Header>¿Que acuario necesita un goldfish para vivir?</Accordion.Header>
-                                <Accordion.Body>
-                                    Cada ejemplar de esta especie requiere como mínimo unos 40 litros de agua para satisfacer todas sus necesidades. Con un acuario de un tamaño de 50 cm de ancho x 40 de alto y 30 cm de fondo será suficiente para tener un único pez carpa. Intenta elegir el acuario de tamaño grande más compatible con tu espacio disponible, siempre teniendo en cuenta las medidas mínimas que requiere un solo ejemplar.
-                                </Accordion.Body>
-                            </Accordion.Item>
-                            <Accordion.Item eventKey="2">
-                                <Accordion.Header>¿como saber si mi pájaro está enfermo?</Accordion.Header>
-                                <Accordion.Body>
-                                    La gran mayoría de especies exóticas tienen un metabolismo basal más acelerado del que estamos acostumbrados respecto a perros o gatos. Las aves son uno de esos grupos. Cuando un pájaro está enfermo hay ciertas características que son más fáciles de ver y que, en todo caso, siempre van a necesitar una atención inmediata por el veterinario especialista en Gwana Vet:
-
-                                    <ul>
-                                        <li>Lleva tiempo sin comer o selecciona la comida</li>
-                                        <li>No se mueve de algún lugar de su jaula, ya sea en una percha o en el suelo</li>
-                                        <li>Ahueca las plumas en postura “embolada” o tiene temblores</li>
-                                        <li>Ha dejado de cantar o emitir sus sonidos habituales</li>
-                                        <li>Respira de forma agitada o con el pico abierto</li>
-                                        <li>Se mueve siempre con el mismo patrón repetitivo por la jaula</li>
-                                        <li>Se pica las plumas o las patas con demasiada frecuencia</li>
-                                        <li>Las heces son más blandas o han cambiado de color</li>
-
-                                    </ul>
-                                </Accordion.Body>
-                            </Accordion.Item>
-                            <Accordion.Item eventKey="3">
-                                <Accordion.Header>¿Cómo saber si mi hurón está enfermo?</Accordion.Header>
-                                <Accordion.Body>
-                                    Este simpático y activo animal muestra los síntomas de enfermedad de manera muy aguda, por lo que deberás llamar o presentarte en el veterinario especialista cuanto antes. Los síntomas más fácilmente reconocibles son los siguientes:
-                                    <ul>
-                                        <li>Está inactivo y muy apático, apenas quiere salir de su escondite o de la hamaca</li>
-                                        <li>Se queja cuando lo coges</li>
-                                        <li>Arrastra la parte posterior del cuerpo al caminar</li>
-                                        <li>No está comiendo</li>
-                                        <li>Le cuesta defecar o sus heces son blandas</li>
-                                        <li>Se rasca el cuerpo más de lo habitual</li>
-                                        <li>Tiene náuseas, hace como que va a vomitar</li>
-                                    </ul>
-                                </Accordion.Body>
-                            </Accordion.Item>
-                        </Accordion>
-                        <Link to="/questions" className="button-faqs">Saber más</Link>
-                    </div>
-                </Col>
-            </Row> */}
 
             <div className="plansText">
                 <h2><span className="sharedlovetxt1">Planes económicos </span><span className="sharedlovetxt2">para tus peluditos</span></h2>
--- src/Pages/QuestionsPage/QuestionsPage.jsx
+++ src/Pages/QuestionsPage/QuestionsPage.jsx
@@ -1,10 +1,23 @@
 import { Container, Accordion, Row} from "react-bootstrap"
 import './QuestionsPage.css'
 
+import { useEffect } from "react"
+import { useLocation } from "react-router-dom"
 
 const QuestionsPage = () => {
 
+ const location = useLocation()
 
+  useEffect(() => {
+    if (location.hash) {
+      const element = document.getElementById(location.hash.replace("#", ""))
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" })
+      }
+    }
+  }, [location])
+
+  
     return (
 
         <div className="QestionsPage">
